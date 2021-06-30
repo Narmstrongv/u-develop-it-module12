6f1588fe5d5c1e85b15097e882f1c28b38930482
Newton <narmstrongv@gmail.com>
2021-06-30 09:42:10 -0700

    added table

--- server.js
+++ server.js
@@ -79,7 +79,12 @@ app.delete('/api/candidate/:id', (req, res) => {
 
 // Create a candidate
 app.post('/api/candidate', ({ body }, res) => {
-  const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+  const errors = inputCheck(
+    body, 
+    'first_name', 
+    'last_name', 
+    'industry_connected'
+  );
   if (errors) {
     res.status(400).json({ error: errors });
     return;
@@ -100,18 +105,6 @@ app.post('/api/candidate', ({ body }, res) => {
     });
   });
 });
-
-// Create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-//               VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-
-// db.query(sql, params, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
